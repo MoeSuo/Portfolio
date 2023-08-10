9291f8fd73e283311f61274d07563c0eddec62c0
Mohamed Sallam <mohamed.sallam@learnit.fi>
2023-08-10 11:27:20 +0300

    fix root pointer

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Add the pages configuration here
-  pages: {
-    // The key is the location of the pages directory in your project
-    src: '/src/pages',
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
