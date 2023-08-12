d15d0f8cd62e1bae72d97de02b2a931bf8df1a74
Mohamed Sallam <mohamed.sallam@learnit.fi>
2023-08-12 17:48:02 +0300

    Add phone number

--- src/pages/components/nav/nav.jsx
+++ src/pages/components/nav/nav.jsx
@@ -141,7 +141,7 @@ export default function NavBar() {
                       <Menu.Item>
                         {({ active }) => (
                           <Link
-                            href="https://api.whatsapp.com/send?phone=1234567890"
+                            href="https://api.whatsapp.com/send?phone=358452548077"
                             className={classNames(
                               active ? "bg-gray-100" : "",
                               "block px-4 py-2 text-sm text-gray-700"
