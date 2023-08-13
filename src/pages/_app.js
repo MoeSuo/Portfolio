64da07a12788d65fe09e76a75de7efc041393b65
Mohamed Sallam <mohamed.sallam@learnit.fi>
2023-08-13 14:16:47 +0300

    sendinblue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -54,32 +54,18 @@ function App({ Component, pageProps }) {
 
 
   {/* <Sendinblue />  */}
-<Script id="google-tag-manager" strategy="afterInteractive">
+<Script id="google-tag-manager" strategy="afterInteractive" type="text/javascript">
       {`
-        (function() {
-    window.sib = {
-        equeue: [],
-        client_key: "c0p4p2p3l3gzyzywcltxgdsg"
-    };
-    /* OPTIONAL: email for identify request*/
-    // window.sib.email_id = 'example@domain.com';
-    window.sendinblue = {};
-    for (var j = ['track', 'identify', 'trackLink', 'page'], i = 0; i < j.length; i++) {
-    (function(k) {
-        window.sendinblue[k] = function() {
-            var arg = Array.prototype.slice.call(arguments);
-            (window.sib[k] || function() {
-                    var t = {};
-                    t[k] = arg;
-                    window.sib.equeue.push(t);
-                })(arg[0], arg[1], arg[2], arg[3]);
-            };
-        })(j[i]);
-    }
-    var n = document.createElement("script"),
-        i = document.getElementsByTagName("script")[0];
-    n.type = "text/javascript", n.id = "sendinblue-js", n.async = !0, n.src = "https://sibautomation.com/sa.js?key=" + window.sib.client_key, i.parentNode.insertBefore(n, i), window.sendinblue.page();
-})();
+        (function(d, w, c) {
+          w.BrevoConversationsID = '64d8b8873ecdf44215688032';
+          w[c] = w[c] || function() {
+              (w[c].q = w[c].q || []).push(arguments);
+          };
+          var s = d.createElement('script');
+          s.async = true;
+          s.src = 'https://conversations-widget.brevo.com/brevo-conversations.js';
+          if (d.head) d.head.appendChild(s);
+      })(document, window, 'BrevoConversations');
       `}
     </Script>
 </Head>
