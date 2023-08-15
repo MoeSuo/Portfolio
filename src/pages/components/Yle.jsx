2cb8e6f4476779e62aff421022a44717cbd79922
Mohamed Sallam <33803711+MoeSuo@users.noreply.github.com>
2023-08-15 16:47:21 +0300

    Update Yle.jsx

--- src/pages/components/Yle.jsx
+++ src/pages/components/Yle.jsx
@@ -8,7 +8,7 @@ export default function Yle() {
 
           <h2 className="text-gray-800 font-bold text-left   mb-4">Latest Project</h2>
         <div className="flex md:flex-row flex-col ">
-          <Link href="https://www.aalto.fi/en/news/can-i-vote-an-online-tool-for-spreading-the-word-about-local-democracy-in-finland">
+          <Link target="_blank" href="https://www.aalto.fi/en/news/can-i-vote-an-online-tool-for-spreading-the-word-about-local-democracy-in-finland">
             <Image
               className="  md:mr-8  "
               src="/yle.svg.png"
@@ -19,7 +19,7 @@ export default function Yle() {
             />
           </Link>
           <div className="flex flex-col items-left mb-8">
-          <Link className="ml-4" href="https://www.aalto.fi/en/news/can-i-vote-an-online-tool-for-spreading-the-word-about-local-democracy-in-finland">
+          <Link target="_blank" className="ml-4" href="https://www.aalto.fi/en/news/can-i-vote-an-online-tool-for-spreading-the-word-about-local-democracy-in-finland">
             CAN I VOTE
           </Link>
           <p className="ml-4">
@@ -30,7 +30,7 @@ export default function Yle() {
 
 
         <div className="flex md:flex-row flex-col">
-          <Link  href="https://main.dps6886imp1cr.amplifyapp.com/">
+          <Link target="_blank"  href="https://main.dps6886imp1cr.amplifyapp.com/">
             <Image
              className="  md:mr-8  "
               src="/garden_logo.png"
@@ -41,7 +41,7 @@ export default function Yle() {
             />
           </Link>
           <div className="flex flex-col items-left mb-8">
-          <Link className="ml-4" href="https://main.dps6886imp1cr.amplifyapp.com/">
+          <Link target="_blank" className="ml-4" href="https://main.dps6886imp1cr.amplifyapp.com/">
           Garden App
           </Link>
           <p className="ml-4">
